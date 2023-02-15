d644aefc3d07ddd02f8994ec7a3861c023bccf48
Matthew Masarik <Matthew.Masarik@noaa.gov>
2023-02-15 21:06:30 +0000

    add: scotch hera build

--- docs/build_scotch_hera.sh
+++ docs/build_scotch_hera.sh
@@ -1,29 +1,49 @@
-#!/bin/bash --login
+#!/bin/bash
 
-# https://gitlab.inria.fr/scotch/scotch.git
+set -eux
 
-BLD_TYPE=Debug
-##BLD_TYPE=Release
-SCOTCH_GZ=scotch.v7.0.3.tar.gz
+BASEDIR="/scratch1/NCEPDEV/climate/Matthew.Masarik/waves/opt/hpc-stack"
+##BUILD_TYPE="Debug"
+BUILD_TYPE="Release"
+CTEST_RUN="NO"
 
-tar xvf $SCOTCH_GZ
-cd scotch
-git checkout v7.0.3
+name="scotch"
+version=${VERSION:-"v7.0.3"}
+INSTALL_PREFIX=${INSTALL_PREFIX:-${BASEDIR}/$name-$version/install}
 
+tar xvf
+
+#[[ -d ${BASEDIR}/$name-$version ]] && rm -rf ${BASEDIR}/$name-$version
+#[[ ! -d ${BASEDIR}/$name-$version ]] && git clone -b $version https://gitlab.inria.fr/scotch/scotch.git ${BASEDIR}/$name-$version
+[[ ! -d ${BASEDIR}/$name-$version ]] && ( echo "${BASEDIR}/$name-$version does not exist, ABORT!"; exit 1)
+
+set +x
 module purge
 module load cmake/3.20.1
-module load gnu/9.2.0
 module load intel/2022.1.2
 module load impi/2022.1.2
 module use  /scratch1/NCEPDEV/nems/role.epic/hpc-stack/libs/intel-2022.1.2/modulefiles/stack
 module load hpc/1.2.0
 module load hpc-intel/2022.1.2
 module load hpc-impi/2022.1.2
+module load gnu/9.2.0
+module list
+set -x
 
-mkdir build && cd build
-cmake -DCMAKE_Fortran_COMPILER=ifort            \
-      -DCMAKE_C_COMPILER=icc                    \
-      -DCMAKE_INSTALL_PREFIX=install            \
-      -DCMAKE_BUILD_TYPE=$BLD_TYPE ..           |& tee cmake.out
-make  VERBOSE=1                                 |& tee  make.out
-make  install
+export CC=mpiicc     # icc makes no difference as hpc-impi is loaded
+export FC=mpiifort   # ifort makes no difference as hpc-impi is loaded
+
+[[ -d $INSTALL_PREFIX ]] && rm -rf $INSTALL_PREFIX
+
+BUILD_DIR=${BUILD_DIR:-"$name-$version/build"}
+[[ ${BUILD_CLEAN:-"YES"} = "YES" ]] && rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+
+cmake ${BASEDIR}/$name-$version \
+      ${CMAKE_OPTIONS:-} \
+      -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE:-"Release"} \
+      -DTHREADS=OFF
+make -j ${BUILD_JOBS:-1} VERBOSE=${BUILD_VERBOSE:-}
+[[ ${CTEST_RUN:-"NO"} = "YES" ]] && ctest -j ${CTEST_JOBS:-1}
+make install
